b856525d160b9ed822c1417de4c278513b8a8421
Yana <cahyana@tass.telkomuniversity.ac.id>
2024-02-28 16:04:41 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/org/d3ifxxxx/helloandroid/MainActivity.kt
+++ app/src/main/java/org/d3ifxxxx/helloandroid/MainActivity.kt
@@ -1,22 +1,14 @@
 package org.d3ifxxxx.helloandroid
 
-import android.content.res.Configuration
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import org.d3ifxxxx.helloandroid.ui.screen.MainScreen
 import org.d3ifxxxx.helloandroid.ui.theme.HelloAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,49 +21,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit){
-    Scaffold (
-        topBar = {
-            TopAppBar(title = { 
-                Text(text = stringResource(id = R.string.app_name))
-            },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        }
-    ){paddingValues -> content(Modifier.padding(paddingValues))
 
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    MainScreen {
-        modifier ->
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
 
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun ScreenPreview() {
-    HelloAndroidTheme {
-        Greeting("Android")
-    }
-}
